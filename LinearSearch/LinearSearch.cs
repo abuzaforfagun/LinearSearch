12996ec018f36d2299dec31183ea468ee58fe9c6
Abu Zafor Fagun <mdabuzaforfagun@gmail.com>
2017-12-02 02:57:17 +0600

    Implement linear search

---
+++ LinearSearch/LinearSearch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearSearch
+{
+    public class LinearSearch
+    {
+        public static int DoLinearSearch(int[] numbers, int number)
+        {
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == number)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
---
+++ LinearSearch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 2, 34, 19, 38, 1, 28, 20 };
+            Console.WriteLine(LinearSearch.DoLinearSearch(numbers, 19));
+            Console.Read();
+        }
+    }
+}
